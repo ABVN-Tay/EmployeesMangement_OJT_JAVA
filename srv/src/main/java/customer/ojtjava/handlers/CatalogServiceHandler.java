ef044a48a0d7e4890a3ed0028358d1080b18bc62
ABVN-Tay <tango@abeam.com>
2025-07-07 09:09:45 +0000

    [Service] Modify service handler

--- srv/src/main/java/customer/ojtjava/handlers/CatalogServiceHandler.java
+++ srv/src/main/java/customer/ojtjava/handlers/CatalogServiceHandler.java
@@ -1,26 +1,24 @@
 package customer.ojtjava.handlers;
 
-import java.util.stream.Stream;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import com.sap.cds.services.cds.CqnService;
 import com.sap.cds.services.handler.EventHandler;
-import com.sap.cds.services.handler.annotations.After;
 import com.sap.cds.services.handler.annotations.ServiceName;
+import com.sap.cds.services.persistence.PersistenceService;
 
 import cds.gen.catalogservice.CatalogService_;
-import cds.gen.catalogservice.Books;
 
 @Component
 @ServiceName(CatalogService_.CDS_NAME)
 public class CatalogServiceHandler implements EventHandler {
+	 private static final Logger logger = LoggerFactory.getLogger(CatalogServiceHandler.class);
+	 private PersistenceService db;
 
-	@After(event = CqnService.EVENT_READ)
-	public void discountBooks(Stream<Books> books) {
-		books.filter(b -> b.getTitle() != null && b.getStock() != null)
-		.filter(b -> b.getStock() > 200)
-		.forEach(b -> b.setTitle(b.getTitle() + " (discounted)"));
-	}
+    public CatalogServiceHandler(PersistenceService db) {
+        this.db = db;
+    }
 
+    
 }
